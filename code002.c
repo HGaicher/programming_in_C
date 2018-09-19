31d0cd557afc5997f6bbeb0ee2824c1e3d9ea7ba
HGaicher <43340698+HGaicher@users.noreply.github.com>
2018-09-19 13:23:22 +0200

    Beispiel Escape-Sequenzen

---
+++ code002.c
@@ -0,0 +1,13 @@
+/* code 002 */
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");		// neue Zeile
+	printf("Hello\n world!\n");		// 2x neue Zeile
+	printf("Hello\t world!\n");		// Tabulator
+	printf("Hello\b world!\n");		// Backspace
+	printf("Hello\r world!\n");		// Carriage return
+	printf("Hello\a world!\n");		// Beep
+	printf("Hello\007 world!\n");	// Beep
+}
