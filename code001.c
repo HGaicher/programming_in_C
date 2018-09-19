0b8551f88a6663269ac2172a4abea40ee8a3e999
HGaicher <43340698+HGaicher@users.noreply.github.com>
2018-09-19 10:26:28 +0200

    comments in C
    first example of comments in C

---
+++ code001.c
@@ -0,0 +1,16 @@
+/* code 001 */
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");       //Bildschirmausgabe -> Hello world
+
+    getchar();
+    /* Die Funktion getchar()
+     * dient zum zeichenweisen
+     * Einlesen von Zeichen von der Standardeingabe
+     */
+
+    //Bildschirmausgabe -> Ich lerne C!
+    printf("Ich lerne C!\n");
+}
