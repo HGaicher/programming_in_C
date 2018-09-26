b797332183f6ec429412e901319d70b9d24ac26d
HGaicher <43340698+HGaicher@users.noreply.github.com>
2018-09-26 11:27:25 +0200

    Variablen definition

---
+++ code003.c
@@ -0,0 +1,11 @@
+/* code 003 */
+#include <stdio.h>
+
+int main()
+{
+     int x,y,z;			// Definition von x, y und z
+
+     printf("x = %i\n",x);
+     printf("y = %i\n",y);
+     printf("z = %i\n",z);
+}
