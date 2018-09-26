55848c403693f8cbe44f2a9655efbf7a53d143d3
Heimo Gaicher <43340698+HGaicher@users.noreply.github.com>
2018-09-26 10:57:35 +0200

    Update and rename code004.c to code004-1.c

---
+++ code004-1.c
@@ -0,0 +1,13 @@
+/* code 004-1 */
+
+#include <stdio.h>
+
+int x, y;                   	// Definition von x und y
+
+void addiere(void);         	// Deklaration der Funktion addiere welche sich in add.c befindet
+
+int main(void)              	// Definition der Funktion main
+{
+     addiere();             	// Aufruf der Funktion addiere in add.c
+     printf("Summe = %d\n",x);	// Ausgabe von x, Summe = 10
+}
--- code004.c
+++
@@ -1,9 +0,0 @@
-/* code 004 */
-#include <stdio.h>
-
-int main (void)					
-{          
-	int a=10, b=2, sum=0;
-	sum = a + b;
-	printf("Die Summe aus %i + %i = %i\n",a ,b ,sum);	
-}
